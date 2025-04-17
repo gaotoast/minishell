cd27dc16e3b13c2321c6882558130cb58afd8e51
stakada <stakada@student.42tokyo.jp>
2025-04-17 10:46:24 +0900

    fix: パス検索でのft_strjoin失敗時のエラー処理

--- srcs/execution/find_path.c
+++ srcs/execution/find_path.c
@@ -1,42 +1,53 @@
 #include "minishell.h"
 
 // 環境変数PATHの値を一つずつ確認
-char	*search_path(char *cmd_name, char **path_list)
+char	*search_path(char *cmd_name, char **path_list, char *path_tail)
 {
 	char	*path;
-	char	*path_tail;
 	int		i;
 
 	i = 0;
 	while (path_list[i])
 	{
-		path_tail = ft_strjoin("/", cmd_name);
 		path = ft_strjoin(path_list[i], path_tail);
-		free(path_tail);
+		if (!path)
+		{
+			perror("minishell");
+			return (NULL);
+		}
 		if (access(path, F_OK) == 0)
 			return (path);
 		free(path);
 		i++;
 	}
+	ft_dprintf(STDERR_FILENO, "%s: command not found\n", cmd_name);
 	return (NULL);
 }
 
 // 環境変数PATHからコマンドのパスを探す
 char	*resolve_cmd_path(char *cmd, char *path_env)
 {
+	char	*path_tail;
 	char	**path_list;
 	char	*filepath;
 
-	path_list = ft_split(path_env, ':');
-	if (!path_list)
+	path_tail = ft_strjoin("/", cmd);
+	if (!path_tail)
 	{
 		perror("minishell");
 		return (NULL);
 	}
-	filepath = search_path(cmd, path_list);
+	path_list = ft_split(path_env, ':');
+	if (!path_list)
+	{
+		perror("minishell");
+		free(path_tail);
+		return (NULL);
+	}
+	filepath = search_path(cmd, path_list, path_tail);
 	if (!filepath)
 	{
-		ft_dprintf(STDERR_FILENO, "%s: command not found\n", cmd);
+		free(path_tail);
 		free_2d_array((void **)path_list);
 		return (NULL);
 	}
