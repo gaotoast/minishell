7327c7aca324b5f897975fef78abd67fb2a137df
yumiyao <yumiyao@student.42tokyo.jp>
2025-04-29 03:22:27 +0900

    exportの作成

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,20 +6,17 @@
 /*   By: yumiyao <yumiyao@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 02:47:53 by yumiyao           #+#    #+#             */
-/*   Updated: 2025/04/18 04:22:42 by yumiyao          ###   ########.fr       */
+/*   Updated: 2025/04/29 03:22:02 by yumiyao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
+#include "minishell.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <errno.h>
-# include <limits.h>
-# include <linux/limits.h>
-# include <string.h>
-
+/**
+ * １つの環境変数を
+ * declare -x <NAME>=<VALUE>\n
+ * の形で表示する
+ */
 void	print_env(char *env)
 {
 	int	i;
@@ -32,7 +29,7 @@ void	print_env(char *env)
 	if (env[i])
 	{
 		write(STDOUT_FILENO, "=\"", 2);
-		write(STDOUT_FILENO, env + i, strlen(env + i));
+		write(STDOUT_FILENO, env + i, ft_strlen(env + i));
 		write(STDOUT_FILENO, "\"", 1);
 	}
 	write(STDOUT_FILENO, "\n", 1);
@@ -45,12 +42,13 @@ int	print_envs(char **envp, int len)
 	int		j;
 
 	i = 0;
+	// ascii順にソート
 	while (i < len)
 	{
 		j = i + 1;
 		while (j < len - 1)
 		{
-			if (strcmp(envp[i], envp[j]) > 0)
+			if (ft_strncmp(envp[i], envp[j], ft_strlen(envp[i])) > 0)
 			{
 				tmp = envp[i];
 				envp[i] = envp[j];
@@ -60,29 +58,151 @@ int	print_envs(char **envp, int len)
 		}
 		++i;
 	}
-	while (*envp)
-		print_env(*(envp++));
+	i = 0;
+	while (envp[i])
+		print_env(envp[i++]);
+	return (0);
+}
+
+int	add_bottom_env(char *str, char ***envp, int len)
+{
+	char	**rtn;
+	int		i;
+
+	rtn = (char **)malloc(sizeof(char *) * (len + 2));
+	if (!rtn)
+	{
+		perror("minishell");
+		return (2);
+	}
+	i = 0;
+	while (i < len)
+	{
+		rtn[i] = (*envp)[i];
+		++i;
+	}
+	rtn[i] = str;
+	rtn[i + 1] = NULL;
+	free(*envp);
+	*envp = rtn;
+	return (0);
+}
+
+int	overwrite_env(char *str, char ***envp, int idx)
+{
+	char	*rtn;
+
+	rtn = ft_strdup(str);
+	if (!rtn)
+	{
+		perror("minishell");
+		return (2);
+	}
+	free((*envp)[idx]);
+	(*envp)[idx] = rtn;
+	return (0);
+}
+
+int	get_eq_idx(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!isalpha(str[0]) || str[0] != '_')
+		return (-1);
+	while (str[i] != '=')
+	{
+		if (isalnum(str[i]) || str[i] == '_')
+			++i;
+		else
+			return (-1);
+	}
+	return (i);
+}
+
+int	set_input(char *str, char ***envp, int len)
+{
+	int	i;
+	int	j;
+	//TODO エラー処理
+
+	i = get_eq_idx(str);
+	j = 0;
+	if (i == -1)
+		return (1);
+	write(1, str, i + 1);
+	while (j < len)
+	{
+		if (ft_strncmp((*envp)[j], str, i + 1))
+			++j;
+		else
+			break ;
+	}
+	if (j != len)
+		return (overwrite_env(str, envp, j));
+	else
+		return (add_bottom_env(str, envp, len));
+}
+
+int	export(int argc, char **argv, char ***envp)
+{
+	int	len;
+	int	i;
+	int	rtn;
+	int	error;
+
+	len = 0;
+	while ((*envp)[len])
+		++len;
+	if (argc == 1)
+		return (print_envs(*envp, len));
+	i = 1;
+	rtn = 0;
+	error = 0;
+	while (i < argc && argv[i])
+	{
+		rtn = set_input(argv[i], envp, len);
+		if (rtn == 2)
+			return (EXIT_FAILURE);
+		else if (rtn == 1)
+		{
+			write(STDERR_FILENO, "bash: export: `", 15);
+			write(STDERR_FILENO, argv[i], strlen(argv[i]));
+			write(STDERR_FILENO, "' :not a valid identifier\n", 26);
+			error = 1;
+		}
+		++i;
+	}
+	if (error)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
-void	set_input(char **argv, char **envp)
-{
-	return ;
-}
+// char	**copy_env(char **envp)
+// {
+// 	int		i;
+// 	int		len;
+// 	char	**rtn;
 
-int	export(int argc, char **argv, char **envp)
-{
-	int	len;
+// 	len = 0;
+// 	while (envp[len])
+// 		++len;
+// 	rtn = (char **)malloc(sizeof(char *) * (len + 1));
+// 	i = 0;
+// 	while (envp[i])
+// 	{
+// 		rtn[i] = strdup(envp[i]);
+// 		++i;
+// 	}
+// 	rtn[i] = NULL;
+// 	return (rtn);
+// }
 
-	len = 0;
-	while (envp[len])
-		++len;
-	if (argc == 1)
-		return (print_envs(envp, len));
-	set_input(argv, envp);
-}
-
-int main(int argc, char **argv, char **envp)
-{
-	export(argc, argv, envp);
-}
+// int main(int argc, char **argv, char **envp)
+// {
+// 	char	**cenv = copy_env(envp);
+// 	// while (*cenv)
+// 	// 	printf("%s\n", *cenv++);
+// 	export(argc, argv, &cenv);
+// 	export(1, argv, &cenv);
+// }
