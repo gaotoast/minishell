1ec54a72b4919570a89d264861fea20b792dee88
stakada <160503699+gao0325ts@users.noreply.github.com>
2025-05-18 07:38:11 +0900

    heredoc内EOFに対する出力を追加

--- srcs/redirection/heredoc.c
+++ srcs/redirection/heredoc.c
@@ -39,7 +39,12 @@ int	write_heredoc_input(char *temp_file, t_redir *redir)
 	{
 		line = readline("> ");
 		if (!line)
+		{
+			ft_dprintf(STDERR_FILENO,
+				"minishell: warning: here-document delimited by end-of-file (wanted `%s')\n",
+				redir->str);
 			break ;
+		}
 		if (ft_strncmp(line, redir->str, ft_strlen(redir->str)) == 0)
 		{
 			free(line);
@@ -49,7 +54,7 @@ int	write_heredoc_input(char *temp_file, t_redir *redir)
 		write(temp_fd, "\n", 1);
 		free(line);
 	}
-    close(temp_fd);
+	close(temp_fd);
 	return (0);
 }
 
@@ -77,10 +82,10 @@ int	get_heredoc_fd(t_redir *redir, int index)
 	if (create_temp_file(index, &temp_file) != 0)
 		return (-1);
 	if (write_heredoc_input(temp_file, redir) != 0)
-    {
-        free(temp_file);
-        return (-1);
-    }
+	{
+		free(temp_file);
+		return (-1);
+	}
 	read_fd = open_and_unlink_temp(temp_file);
 	if (read_fd < 0)
 	{
