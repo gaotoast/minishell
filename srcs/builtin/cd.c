df4f343213e25deffbc039e549034e17be0e4e71
yumiyao <yumiyao@student.42tokyo.jp>
2025-05-22 11:24:48 +0900

    メモリリーク修正

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: yumiyao <yumiyao@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 03:40:56 by yumiyao           #+#    #+#             */
-/*   Updated: 2025/05/21 18:27:54 by yumiyao          ###   ########.fr       */
+/*   Updated: 2025/05/22 11:22:44 by yumiyao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,6 @@ char	*ft_union(char **split, char delim)
 	if (!rtn)
 		return (NULL);
 	i = 0;
-	rtn[len] = '\0';
 	len = 0;
 	while (split[i])
 	{
@@ -166,7 +165,7 @@ char	*get_abs_path(char *dest)
 	inner_split = make_abs_path(dest_split, inner_split, inner_len);
 	rtn = ft_union(inner_split, '/');
 	free_2d_array(inner_split);
-	free_2d_array(dest_split);
+	free(dest_split);
 	return (rtn);
 }
 
