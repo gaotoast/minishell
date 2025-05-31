538e77c55fd7caa44f6353dd18ecb37427ad3c19
yumiyao <yumiyao@student.42tokyo.jp>
2025-06-01 03:49:26 +0900

    ft_dprintfを使う

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: yumiyao <yumiyao@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 00:20:46 by yumiyao           #+#    #+#             */
-/*   Updated: 2025/06/01 03:30:04 by yumiyao          ###   ########.fr       */
+/*   Updated: 2025/06/01 03:46:06 by yumiyao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,12 @@ int	echo(int argc, char **argv)
 	i = check_argv(argv, &endnew);
 	while (i < argc)
 	{
-		printf("%s", argv[i++]);
+		ft_dprintf(STDOUT_FILENO, "%s", argv[i++]);
 		if (i < argc)
-			printf(" ");
+			ft_dprintf(STDOUT_FILENO, " ");
 	}
 	if (endnew)
-		printf("\n");
+		ft_dprintf(STDOUT_FILENO, "\n");
 	return (EXIT_SUCCESS);
 }
 
